ba77867c985d987b0f55623a2087308feab31bff
Yuka Itozu <yukaitozu@gmail.com>
2020-04-01 22:24:56 +0900

    updated controllers

--- app/controllers/character_controller.rb
+++ app/controllers/character_controller.rb
@@ -1,4 +1,29 @@
 class CharacterController < ApplicationController
-  def index
+  def new
+    @episode = Episode.find(params[:episode_id])
+    @character = Character.new
+  end
+
+  def create
+    @episode = Episode.find(params[:episode_id])
+    @character = Character.new(character_params)
+    @character.episode = @episode
+    if @character.save
+      redirect_to episode_path(@episode)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @character = character.find(params[:id])
+    @character.destroy
+    redirect_to episode_path(@character.episode)
+  end
+
+  private
+
+  def character_params
+    params.require(:character).permit(:first_name, :last_name, :quote)
   end
 end
--- app/controllers/episode_controller.rb
+++ app/controllers/episode_controller.rb
@@ -1,5 +1,38 @@
 class EpisodeController < ApplicationController
-  def index
-
+   def index
+    @episodes = Episode.all
   end
+
+  def show
+    @episode = Episode.find(params[:id])
+    @path = ''
+  end
+
+  def new
+    @episode = Episode.new
+  end
+
+  def create
+    @episode = Episode.new(episode_params)
+    if @episode.save
+      redirect_to episode_path(@episode)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @episode = episode.find(params[:id])
+    @episode.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def episode_params
+    params.require(:episode).permit(:name, :photo)
+  end
+end
+
+
 end
