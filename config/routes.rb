14f38a4dc29e847a333c0402fa9c8996c7ef43bf
Yuka Itozu <yukaitozu@gmail.com>
2020-04-07 22:39:09 +0900

    made episodes index as home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'episodes#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :episodes do
     resources :characters, only: [:new, :create]
